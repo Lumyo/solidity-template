727018791ec521a8aa41cfec26d0688b2a506e91
Lumyo <lumyo.b@gmail.com>
2022-10-18 18:08:29 +0200

    Update chai matchers

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import Dotenv from 'dotenv';
 // Hardhat plugins
-import '@nomiclabs/hardhat-waffle';
+import '@nomicfoundation/hardhat-chai-matchers';
+import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-etherscan';
 import '@typechain/hardhat';
 import 'hardhat-abi-exporter';
